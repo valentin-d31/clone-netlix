7de9344a957908b6476e8fc4860901dc8ceaa15f
Valentin <valentinarpin11@live.fr>
2023-02-06 11:19:29 +0100

    [Add]: init commit

---
+++ src/firebase.jsx
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAgffUG09RfuaFBrN82vRqOQ927ENGjPa0",
+    authDomain: "netflix-clone-b093c.firebaseapp.com",
+    projectId: "netflix-clone-b093c",
+    storageBucket: "netflix-clone-b093c.appspot.com",
+    messagingSenderId: "838982452480",
+    appId: "1:838982452480:web:2cdda027073c11bc6d9fe5"
+};
