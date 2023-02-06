7cac8764c912a9aca4ef58f2124c1739b3f55708
Valentin <valentinarpin11@live.fr>
2023-02-06 15:46:20 +0100

    [Add]: api_key

---
+++ src/Requests.jsx
@@ -0,0 +1,15 @@
+const API_KEY = "f81980ff410e46f422d64ddf3a56dddd";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchDocumentariesMovies: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+//axios me permet de memoriser l'API_KEY
+//https://api.themoviedb.org/3/trending/all/week?api_key=f81980ff410e46f422d64ddf3a56dddd&language=en-US
