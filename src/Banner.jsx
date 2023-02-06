496a3f1f2aa10c098b87332686308fb57a901ea8
Valentin <valentinarpin11@live.fr>
2023-02-06 16:37:58 +0100

    [Add]: Row component

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -32,21 +32,22 @@ export default function Banner() {
         <header className="banner"
                 style={{
             backgroundSize: "cover",
-                    //retourne la valeur data.backdrop_path
+                    //1.1 retourne la valeur data.backdrop_path
             backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.
             backdrop_path}")`,
             backgroundPosition: "center center",
                 }}
         >
             <div className="banner__contents">
-                <h1 className="banner__title">Movie Name</h1>
+                <h1 className="banner__title">
+                    {movie?.title || movie?.name
+                    || movie?.original_name}</h1>
                 <div className="banner__buttons">
                     <button className="banner__button">Play</button>
                     <button className="banner__button">My List</button>
                 </div>
                 <h1 className="banner__description">
-                    {truncate(`thisisisisisisis`,
-                        150)}
+                    {truncate(movie?.overview, 150)}
                 </h1>
             </div>
 
---
+++ src/Row.jsx
@@ -0,0 +1,9 @@
+import "./Row.css"
+
+export default function Row({title}) {
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+        </div>
+    )
+}
