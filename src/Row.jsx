61d6555f0516ce0afeaec30f48ebc92171d19902
Valentin <valentinarpin11@live.fr>
2023-02-06 17:12:48 +0100

    [Add]: Movies on Row css

--- src/Row.jsx
+++ src/Row.jsx
@@ -1,4 +1,4 @@
-import "./Row.css"
+import './Row.css'
 import {useEffect, useState} from "react";
 import axios from "./axios";
 
@@ -6,6 +6,8 @@ export default function Row({title, fetchUrl, isLargeRow = false}) {
 
     //1.2 fetchUrl->chemin contient la requete
     const [movies, setMovies] = useState([]);
+
+    const base_url = "https://image.tmdb.org/t/p/original";
 
     useEffect(() => {
         async function fetchData() {
@@ -17,11 +19,27 @@ export default function Row({title, fetchUrl, isLargeRow = false}) {
         fetchData()
     }, [fetchUrl])
 
-    console.log(movies);
+    //console.log(movies);
 
     return (
         <div className="row">
             <h2>{title}</h2>
+
+            <div className="row__posters">
+                {movies.map(movie => (
+                    isLargeRow && movie.poster_path ||
+                        !isLargeRow && movie.backdrop_path && (
+                        <img
+                            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                            key={movie.id}
+                            src={`${base_url}${
+                                isLargeRow ? movie.poster_path : movie.backdrop_path
+                            }`}
+                            alt={movie.name}
+                        />
+                    )
+                ))}
+            </div>
         </div>
     )
 }
